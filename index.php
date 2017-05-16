cd4caac9d23969b05bfce41d8d6e274c7993c2b7
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2017-05-16 11:05:56 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD Clientes</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="row main align-middle">
+		<div class="small-10 medium-5 large-4 small-centered medium-centered large-centered columns">
+			<form class="log-in-form">
+				<h4 class="text-center">LOGIN</h4>
+				<label>
+					Email
+				  <input type="email" placeholder="somebody@example.com">
+				</label>
+				<label>
+					Senha
+				  <input type="password" placeholder="Password">
+				</label>
+				<input id="show-password" type="checkbox">
+				<label for="show-password">
+					Lembrar minhas credenciais
+				</label>
+				<p>
+					<input type="submit" class="button expanded" value="Logar">
+				</p>
+			</form>
+	  </div>
+	</div>
+</body>
+</html>
---
+++ model/conn.php
@@ -0,0 +1,3 @@
+<?php  
+	$pdo = new PDO('mysql:host=localhost;dbname=crud;charset=UTF-8', 'root', 'root');
+?>
